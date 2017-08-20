8990c152b567913537043cb56d9c0914e3235a08
wenjun <wen911119@163.com>
2017-08-21 00:56:47 +0800

    commit

---
+++ NumberInput/app.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" @blur="$emit('blur')" @focus="$emit('focus')" v-model="inputValue">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'uint'
+        },
+        value: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            inputValue: this.value
+        }
+    },
+    watch: {
+        inputValue: function (nv) {
+            this.inputValue = nv.replace(/[^\d]/g, '')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
