cfec8b1c0ef4b684b2f663a49ba8591a2eabf5a9
wenjun <wen911119@163.com>
2017-08-22 01:20:42 +0800

    NumberInput 多合一

--- NumberInput/app.vue
+++ NumberInput/app.vue
@@ -9,12 +9,29 @@ export default {
     props: {
         type: {
             type: String,
-            default: 'uint'
+            default: 'ufloat' // uint ufloat int float
         },
         value: {
             type: Number,
-            default: 0
+            required: false
+        },
+        maxLength: {
+            type: Number,
+            default: 999
+        },
+        split: {
+            type: Number,
+            default: 4
+        },
+        delimiter: {
+            type: String,
+            default: ''
+        },
+        decimalLength: {
+            type: Number,
+            default: 3
         }
+
     },
     data() {
         return {
@@ -23,7 +40,29 @@ export default {
     },
     watch: {
         inputValue: function (nv) {
-            this.inputValue = nv.replace(/[^\d]/g, '')
+            if (this.type == 'uint') {
+                nv = nv.replace(/[^\d]/g, '')
+                if (nv.length > this.maxLength) {
+                    nv = nv.substring(0, this.maxLength)
+                }
+
+                if (this.delimiter) {
+                    let reg = new RegExp("(\\d{" + this.split + "})", "g")
+                    if (nv.length % this.split == 0) {
+                        nv = nv.replace(reg, "$1" + this.delimiter)
+                        nv = nv.substring(0, nv.length - 1)
+                    } else {
+                        nv = nv.replace(reg, "$1" + this.delimiter)
+                    }
+                }
+            } else if (this.type == 'ufloat') {
+                nv = nv.replace(/[^\d\.]/g, '')
+                nv = nv.replace(/(\.\d+)\./g, '$1')
+                nv = nv.replace(/\.\./g, '.')
+                let reg = new RegExp("(\\d+\\.\\d{" + this.decimalLength + "})\\d+", "g")
+                nv = nv.replace(reg, '$1')
+            }
+            this.inputValue = nv
         }
     }
 }
