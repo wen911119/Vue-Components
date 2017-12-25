c0553e14403db1ec9c5b5b39e1c3f9277356458c
wen911119 <wen911119@gmail.com>
2017-12-25 23:05:52 +0800

    update demo

--- SlotsPanel/demo.vue
+++ SlotsPanel/demo.vue
@@ -5,6 +5,11 @@
             <span slot="slot-1">wee</span>
             <span>wee3</span>
         </panel>
+        <panel gap=10 >
+            <span slot="slot-2">wee2</span>
+            <span slot="slot-1">wee</span>
+            <span>wee3</span>
+        </panel>
     </div>
 </template>
 <script>
